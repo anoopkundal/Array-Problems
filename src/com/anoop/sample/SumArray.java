4859c76938e55243ce909ab230cc6b2ece2d91e5
Anoop Roy Kundal <anoop@dotvik.com>
2019-09-17 11:14:37 +0530

    Added function to sum the elements into a new array

--- src/com/anoop/sample/SumArray.java
+++ src/com/anoop/sample/SumArray.java
@@ -18,10 +18,18 @@ public class SumArray {
 		System.out.println("Creating Sum Array : " + Arrays.toString(sumInArrayLoop(newArray1)));
 		nano_endTime = System.nanoTime();
 		System.out.println(">>>>> Time taken in nano seconds: " + (nano_endTime - nano_startTime));
+		
+		int [] newArray2 = CommonUtils.generateIncrementedArray(arraySize); // Method in CommonUtils class to generate a random array
+		System.out.println("Original Array : " + Arrays.toString(newArray2));
+		nano_startTime = System.nanoTime(); 
+		System.out.println("Creating New Sum Array : " + Arrays.toString(sumOutArrayLoop(newArray2)));
+		nano_endTime = System.nanoTime();
+		System.out.println(">>>>> Time taken in nano seconds: " + (nano_endTime - nano_startTime));
 	}
 	
 	/*
 	 * Method to sum the elements by looping through the array 
+	 * O(n) problem
 	 */
 	public static int[] sumInArrayLoop(int[] newArray) {
 		// Looping through the and replacing elements with the sum of the previous elements
@@ -29,6 +37,22 @@ public class SumArray {
     	  newArray[i] = newArray[i-1]+newArray[i];
     	}
         return newArray;
+    }
+	
+	/*
+	 * Method to sum the elements by looping through the array 
+	 * O(n) problem
+	 */
+	public static int[] sumOutArrayLoop(int[] newArray) {
+		// Looping through the and creating a new array with the sum of the previous elements
+		int[] sumArray = new int[newArray.length];
+		for(int i=0; i<newArray.length; i++){
+			if(i==0)
+				sumArray[i] = newArray[i];
+			else
+				sumArray[i] = sumArray[i-1] + newArray[i];
+    	}
+        return sumArray;
     }
 
 }
